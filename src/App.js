7b2870a52768977899ed489a85270348b136e1dd
emerymastering <emery@mastering.lt>
2022-08-20 11:49:31 +0200

    testing react app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>Hello again!!</p>
+    </div>
+  );
 }
 
 export default App;
